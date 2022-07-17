ce27bce91b6737dd81b63e66c01cf87d4fe9415a
YuliiaVlasenko <108423239+YuliiaVlasenko@users.noreply.github.com>
2022-07-18 00:11:20 +0300

    added

--- src/index.js
+++ src/index.js
@@ -29,6 +29,45 @@ function formatDay(timestamp) {
 
   return days[day];
 }
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+    
+    let forecastElement = document.querySelector("#forecast");
+    
+    let forecastHTML = `<div class="row">`;
+   
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+       forecastHTML = 
+        forecastHTML + `
+                        <div class="col" id = "forecast-sep">
+                            <div class="weather-forecast-date">
+                                ${formatDay(forecastDay.dt)}
+                            </div>
+                            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" width="42" alt="" />
+                            <div class="weather-forecast-temperature">
+                                <span class="weather-forecast-temperature-max">
+                                    ${Math.round(forecastDay.temp.max)}°
+                                </span>
+                                <span class="weather-forecast-temperature-min">
+                                ${Math.round(forecastDay.temp.min)}°
+                                </span>
+                            </div>
+                        </div>`;} 
+    })
+
+    
+    
+    forecastHTML = forecastHTML + `</div>`;
+    
+    forecastElement.innerHTML = forecastHTML
+}
+
+function getForecast(coordinates) {
+    let apiKey = "38eda62e37f00b901b8d7df72b0503cf";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
